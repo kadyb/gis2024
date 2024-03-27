fffd83d79dbd818a4b011fae5a2ff2b54d44cf2d
Krzysztof Dyba <35004826+kadyb@users.noreply.github.com>
2024-03-27 13:39:06 +0100

    update solution for Zadanie 1

--- zadania/zadanie_1.R
+++ zadania/zadanie_1.R
@@ -6,3 +6,18 @@ sciezka_do_logo # wyświetl
 logo = rast(sciezka_do_logo)
 
 logo
+# class       : SpatRaster
+# dimensions  : 77, 101, 3  (nrow, ncol, nlyr)
+# resolution  : 1, 1  (x, y)
+# extent      : 0, 101, 0, 77  (xmin, xmax, ymin, ymax)
+# coord. ref. : Cartesian (Meter)
+# source      : logo.tif
+# colors RGB  : 1, 2, 3
+# names       : red, green, blue
+# min values  :   0,     0,    0
+# max values  : 255,   255,  255
+
+## raster składa się z trzech kanałów (warstw), tj. czerwonego, zielonego
+## i niebieskiego
+## zakres przestrzenny zdefiniowany jest przez liczbę wierszy (oś Y) i liczbę
+## kolumn (oś X)
