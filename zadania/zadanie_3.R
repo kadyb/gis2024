18253a8e98e5a01eb80ba3911e2a301aa4029a0c
Krzysztof Dyba <35004826+kadyb@users.noreply.github.com>
2024-04-17 14:34:18 +0200

    update solution for Task 3

--- zadania/zadanie_3.R
+++ zadania/zadanie_3.R
@@ -1,40 +1,51 @@
 library("terra")
-raster1=rast("D:/Studia_xD/Rok4/ZAP/Lublin_DEM.tif")
+
+# załóżmy, że folderem roboczym jest "gis2024/zadania", wtedy
+# ścieżka względna to "../dane/Lublin_DEM.tif"
+# alternatywnie można stworzyć projekt w RStudio
+raster=rast("../dane/Lublin_DEM.tif")
 
 #1
-raster1
-plot(raster1)
+raster
+plot(raster)
 
 #2
-zasieg = ext(740000, 752000, 370000,380000 ) # xmin, xmax, ymin, ymax
-r = crop(raster1, zasieg)
-r
-plot(r, ext = ext(raster1)) # porównaj z oryginalnym zasięgiem
+r_2180=project(raster, "EPSG:2180")
+par(mfrow = c(1, 2)) # wyświetl mapy w 2 kolumnach
+plot(raster, main = "World Geodetic System 84")
+plot(r_2180, main = "Układ współrzędnych 1992")
 
 #3
-r_2180=project(raster1, "EPSG:2180")
-par(mfrow = c(1, 2))
-plot(raster1, main = "World Geodetic System 84")
-plot(r_2180, main = "2180")
+par(mfrow = c(1, 1)) # wyświetl tylko 1 mapę
+plot(r_2180)
+# zasieg = draw(x = "extent") # trzeba wskazać dwa wierzchołki na mapie
+# zasieg
+
+# załóżmy, że został wyznaczony taki zasięg w funkcji `draw()`:
+zasieg = ext(740000, 752000, 370000, 380000) # xmin, xmax, ymin, ymax
+r = crop(r_2180, zasieg)
+r
+plot(r, ext = ext(r_2180)) # porównaj z oryginalnym zasięgiem
 
 #4
-rasFocal=focal(raster1, w=3, fun="mean")
+rasFocal=focal(r_2180, w=15, fun="mean")
 par(mfrow = c(1, 2))
-plot(raster1, main = "Raster wejściowy")
+plot(r_2180, main = "Raster wejściowy")
 plot(rasFocal, main = "Raster wygładzony")
 
 #5
-summary(raster1)
+summary(r_2180) # wartości wyliczone z próby
 data.frame(
-  global(raster1, fun="min"),
-  global(raster1, fun="max"),
-  global(raster1, fun="mean"),
-  global(raster1, fun="sd")
+  # wartości wyliczone z populacji
+  global(r_2180, fun="min", na.rm = TRUE),
+  global(r_2180, fun="max", na.rm = TRUE),
+  global(r_2180, fun="mean", na.rm = TRUE),
+  global(r_2180, fun="sd", na.rm = TRUE)
 )
 
 #6
 par(mfrow = c(1, 1))
-plot(raster1, colNA="lightblue", range=c(1,2700), main="Lublin")
+plot(rasFocal, main="Lublin", col = gray.colors(n = 20))
 
 #7
-writeRaster(raster1, filename = "raster.tif", gdal = c("COMPRESS=LZW"))
+writeRaster(rasFocal, filename = "raster.tif", gdal = c("COMPRESS=LZW"))
