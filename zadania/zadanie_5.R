5171ac3ee0dac8383db560735e30868d6a1dd9f8
pgil2207 <92291065+pgil2207@users.noreply.github.com>
2024-06-04 21:08:05 +0200

    Update zadanie_5.R

---
+++ zadania/zadanie_5.R
@@ -0,0 +1,40 @@
+# Załadowanie pakietu
+library(terra)
+
+# Pobranie danych (przykładowy link do granic powiatów)
+url <- "https://www.gis-support.pl/downloads/2022/powiaty.zip"
+download.file(url, "powiaty.zip", mode = "wb")
+wektor <- unzip("powiaty.zip", exdir = "powiaty_dir")
+
+# Wczytanie danych
+f <- "powiaty_dir/powiaty.shp"
+powiaty <- terra::vect(f)
+powiaty
+
+
+#wczytanie danych tekstowych
+dane <- read.table("C:/Users/Paweł/Desktop/zap/malopolskie.txt", sep = " ", dec = ".", header = FALSE)
+str(dane)
+
+coords <- cbind(dane$V1, dane$V2)  # Tworzenie macierzy współrzędnych
+crs <- "+proj=longlat +datum=WGS84"  # Definicja układu współrzędnych
+wysokosc <- dane$V3  # Wysokość
+
+# Utworzenie obiektu SpatVector
+spat_vec <- vect(wysokosc, geom = coords, crs = "EPSG:4326")
+spat_vec
+
+
+#generowanie centroidow siatki
+centroidy = centroids(wektor, inside = FALSE)
+centroidy_wewnatrz = centroids(wektor, inside = TRUE)
+plot(wektor)
+plot(centroidy, add = TRUE, col = "blue")
+plot(centroidy_wewnatrz, add = TRUE, col = "orange")
+
+
+#określenie które centroidy siatki leżą w konkretnym powiecie
+
+proba = spatSample(wektor, size = 5, method = "random")
+plot(wektor)
+plot(proba, add = TRUE)
