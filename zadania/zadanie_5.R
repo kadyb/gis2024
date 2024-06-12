c32a3db7ab266ae11ac788731d9f6efa25ec8e80
Vrokur <115728989+Vrokur@users.noreply.github.com>
2024-06-12 23:18:17 +0200

    Update zadanie_5.R
    :)

--- zadania/zadanie_5.R
+++ zadania/zadanie_5.R
@@ -1,248 +1,91 @@
-# Załadowanie pakietu
+#wykonali Maciej Fornal Hubert Janowski Paweł Gil
 library(terra)
-
-# Pobranie danych (przykładowy link do granic powiatów)
-url <- "https://www.gis-support.pl/downloads/2022/powiaty.zip"
-download.file(url, "powiaty.zip", mode = "wb")
-wektor <- unzip("powiaty.zip", exdir = "powiaty_dir")
-
-# Wczytanie danych
-f <- "powiaty_dir/powiaty.shp"
-powiaty <- terra::vect(f)
-powiaty
-
-
-#wczytanie danych tekstowych
-dane <- read.table("C:/Users/Paweł/Desktop/zap/malopolskie.txt", sep = " ", dec = ".", header = FALSE)
-str(dane)
-
-coords <- cbind(dane$V1, dane$V2)  # Tworzenie macierzy współrzędnych
-crs <- "+proj=longlat +datum=WGS84"  # Definicja układu współrzędnych
-wysokosc <- dane$V3  # Wysokość
-
-# Utworzenie obiektu SpatVector
-spat_vec <- vect(wysokosc, geom = coords, crs = "EPSG:4326")
-spat_vec
-
-
-#generowanie centroidow siatki
-centroidy = centroids(wektor, inside = FALSE)
-centroidy_wewnatrz = centroids(wektor, inside = TRUE)
-plot(wektor)
-plot(centroidy, add = TRUE, col = "blue")
-plot(centroidy_wewnatrz, add = TRUE, col = "orange")
-
-
-#określenie które centroidy siatki leżą w konkretnym powiecie
-
-proba = spatSample(wektor, size = 5, method = "random")
-plot(wektor)
-plot(proba, add = TRUE)
-
-#wczytanie danych tekstowych
-dane <- read.table("*/zachodniopomorskie.txt", sep = " ", dec = ".", header = FALSE)
-str(dane)
-coords <- cbind(dane$V1, dane$V2)  # Tworzenie macierzy współrzędnych
-crs <- "+proj=longlat +datum=WGS84"  # Definicja układu współrzędnych
-wysokosc <- dane$V3  # Wysokość
-
-# Utworzenie obiektu SpatVector
-spat_vec <- vect(wysokosc, geom = coords, crs = "EPSG:4326")
-spat_vec
-
-
-#generowanie centroidow siatki
-centroidy = centroids(wektor, inside = FALSE)
-centroidy_wewnatrz = centroids(wektor, inside = TRUE)
-plot(wektor)
-plot(centroidy, add = TRUE, col = "blue")
-plot(centroidy_wewnatrz, add = TRUE, col = "orange")
-
-
-#określenie które centroidy siatki leżą w konkretnym powiecie
-
-proba = spatSample(wektor, size = 5, method = "random")
-plot(wektor)
-plot(proba, add = TRUE)
-
-#wczytanie danych tekstowych
-dane <- read.table("*/wielkopolskie.txt", sep = " ", dec = ".", header = FALSE)
-str(dane)
-coords <- cbind(dane$V1, dane$V2)  # Tworzenie macierzy współrzędnych
-crs <- "+proj=longlat +datum=WGS84"  # Definicja układu współrzędnych
-wysokosc <- dane$V3  # Wysokość
-# Utworzenie obiektu SpatVector
-spat_vec <- vect(wysokosc, geom = coords, crs = "EPSG:4326")
-spat_vec
-
-
-#generowanie centroidow siatki
-centroidy = centroids(wektor, inside = FALSE)
-centroidy_wewnatrz = centroids(wektor, inside = TRUE)
-plot(wektor)
-plot(centroidy, add = TRUE, col = "blue")
-plot(centroidy_wewnatrz, add = TRUE, col = "orange")
-
-
-#określenie które centroidy siatki leżą w konkretnym powiecie
-
-proba = spatSample(wektor, size = 5, method = "random")
-plot(wektor)
-plot(proba, add = TRUE)
-
-#wczytanie danych tekstowych
-dane <- read.table("*/warminsko-mazurskie.txt", sep = " ", dec = ".", header = FALSE)
-str(dane)
-coords <- cbind(dane$V1, dane$V2)  # Tworzenie macierzy współrzędnych
-crs <- "+proj=longlat +datum=WGS84"  # Definicja układu współrzędnych
-wysokosc <- dane$V3  # Wysokość
-# Utworzenie obiektu SpatVector
-spat_vec <- vect(wysokosc, geom = coords, crs = "EPSG:4326")
-spat_vec
-
-
-#generowanie centroidow siatki
-centroidy = centroids(wektor, inside = FALSE)
-centroidy_wewnatrz = centroids(wektor, inside = TRUE)
-plot(wektor)
-plot(centroidy, add = TRUE, col = "blue")
-plot(centroidy_wewnatrz, add = TRUE, col = "orange")
-
-
-#określenie które centroidy siatki leżą w konkretnym powiecie
-
-proba = spatSample(wektor, size = 5, method = "random")
-plot(wektor)
-plot(proba, add = TRUE)
-
-#wczytanie danych tekstowych
-dane <- read.table("*/swietokrzyskie.txt", sep = " ", dec = ".", header = FALSE)
-str(dane)
-coords <- cbind(dane$V1, dane$V2)  # Tworzenie macierzy współrzędnych
-crs <- "+proj=longlat +datum=WGS84"  # Definicja układu współrzędnych
-wysokosc <- dane$V3  # Wysokość
-# Utworzenie obiektu SpatVector
-spat_vec <- vect(wysokosc, geom = coords, crs = "EPSG:4326")
-spat_vec
-
-
-#generowanie centroidow siatki
-centroidy = centroids(wektor, inside = FALSE)
-centroidy_wewnatrz = centroids(wektor, inside = TRUE)
-plot(wektor)
-plot(centroidy, add = TRUE, col = "blue")
-plot(centroidy_wewnatrz, add = TRUE, col = "orange")
-
-
-#określenie które centroidy siatki leżą w konkretnym powiecie
-
-proba = spatSample(wektor, size = 5, method = "random")
-plot(wektor)
-plot(proba, add = TRUE)
-
-
-
-#określenie które centroidy siatki leżą w konkretnym powiecie
-
-proba = spatSample(wektor, size = 5, method = "random")
-plot(wektor)
-plot(proba, add = TRUE)
-
-#wczytanie danych tekstowych
-dane <- read.table("*/lodzkie", sep = " ", dec = ".", header = FALSE)
-str(dane)
-coords <- cbind(dane$V1, dane$V2)  # Tworzenie macierzy współrzędnych
-crs <- "+proj=longlat +datum=WGS84"  # Definicja układu współrzędnych
-wysokosc <- dane$V3  # Wysokość
-
-# Utworzenie obiektu SpatVector
-spat_vec <- vect(wysokosc, geom = coords, crs = "EPSG:4326")
-spat_vec
-
-
-#generowanie centroidow siatki
-centroidy = centroids(wektor, inside = FALSE)
-centroidy_wewnatrz = centroids(wektor, inside = TRUE)
-plot(wektor)
-plot(centroidy, add = TRUE, col = "blue")
-plot(centroidy_wewnatrz, add = TRUE, col = "orange")
-
-
-#określenie które centroidy siatki leżą w konkretnym powiecie
-
-proba = spatSample(wektor, size = 5, method = "random")
-plot(wektor)
-plot(proba, add = TRUE)
-
-#wczytanie danych tekstowych
-dane <- read.table("*/podlaskie", sep = " ", dec = ".", header = FALSE)
-str(dane)
-coords <- cbind(dane$V1, dane$V2)  # Tworzenie macierzy współrzędnych
-crs <- "+proj=longlat +datum=WGS84"  # Definicja układu współrzędnych
-wysokosc <- dane$V3  # Wysokość
-
-# Utworzenie obiektu SpatVector
-spat_vec <- vect(wysokosc, geom = coords, crs = "EPSG:4326")
-spat_vec
-
-
-#generowanie centroidow siatki
-centroidy = centroids(wektor, inside = FALSE)
-centroidy_wewnatrz = centroids(wektor, inside = TRUE)
-plot(wektor)
-plot(centroidy, add = TRUE, col = "blue")
-plot(centroidy_wewnatrz, add = TRUE, col = "orange")
-
-
-#określenie które centroidy siatki leżą w konkretnym powiecie
-
-proba = spatSample(wektor, size = 5, method = "random")
-plot(wektor)
-plot(proba, add = TRUE)
-
-#wczytanie danych tekstowych
-dane <- read.table("*/dolnoslaskie", sep = " ", dec = ".", header = FALSE)
-str(dane)
-coords <- cbind(dane$V1, dane$V2)  # Tworzenie macierzy współrzędnych
-crs <- "+proj=longlat +datum=WGS84"  # Definicja układu współrzędnych
-wysokosc <- dane$V3  # Wysokość
-
-# Utworzenie obiektu SpatVector
-spat_vec <- vect(wysokosc, geom = coords, crs = "EPSG:4326")
-spat_vec
-
-
-#generowanie centroidow siatki
-centroidy = centroids(wektor, inside = FALSE)
-centroidy_wewnatrz = centroids(wektor, inside = TRUE)
-plot(wektor)
-plot(centroidy, add = TRUE, col = "blue")
-plot(centroidy_wewnatrz, add = TRUE, col = "orange")
-
-
-#określenie które centroidy siatki leżą w konkretnym powiecie
-
-proba = spatSample(wektor, size = 5, method = "random")
-plot(wektor)
-plot(proba, add = TRUE)
-
-#wczytanie danych tekstowych
-dane <- read.table("*/pomorskie", sep = " ", dec = ".", header = FALSE)
-str(dane)
-coords <- cbind(dane$V1, dane$V2)  # Tworzenie macierzy współrzędnych
-crs <- "+proj=longlat +datum=WGS84"  # Definicja układu współrzędnych
-wysokosc <- dane$V3  # Wysokość
-
-# Utworzenie obiektu SpatVector
-spat_vec <- vect(wysokosc, geom = coords, crs = "EPSG:4326")
-spat_vec
-
-
-#generowanie centroidow siatki
-centroidy = centroids(wektor, inside = FALSE)
-centroidy_wewnatrz = centroids(wektor, inside = TRUE)
-plot(wektor)
-plot(centroidy, add = TRUE, col = "blue")
-plot(centroidy_wewnatrz, add = TRUE, col = "orange")
-
+install.packages("rgugik")
+library("rgugik")
+library("rstac")
+install.packages("raster")
+library("raster")
+
+# Funkcja do obliczania mediany i odchylenia standardowego
+calculate_stats <- function(points, regions) {
+  stats_list <- list()
+  for (i in 1:nrow(regions)) {
+    region <- regions[i, ]
+    region_points <- points[region, , drop = FALSE]
+    if (nrow(region_points) > 0) {
+      med <- median(region_points$z, na.rm = TRUE)
+      std <- sd(region_points$z, na.rm = TRUE)
+      region_name <- as.character(region$JPT_NAZWA_)  # Użyj kolumny JPT_NAZWA_
+      if (length(region_name) > 0) {
+        stats_list[[i]] <- data.frame(
+          Wojewodztwo = wojewodztwo,
+          Powiat = region_name,
+          Mediana_wysokosci = med,
+          Odch_Std = std
+        )
+      }
+    }
+  }
+  stats_df <- do.call(rbind, stats_list)
+  return(stats_df)
+}
+
+# Pobieranie repozytorium
+url <- "https://codeload.github.com/Vrokur/Analiza_przestrzenna_5/zip/heads/main"
+download.file(url, "powiaty.zip", mode = "wb", method = "libcurl")
+unzip("powiaty.zip", exdir = "zad5")
+
+# Wczytywanie pliku shapefile powiaty
+powiaty <- vect("zad5/Analiza_przestrzenna_5-heads-main/2/powiaty/powiaty.shp")
+powiaty_2180 <- project(powiaty, "EPSG:2180")
+
+
+
+# Wczytywanie danych dla każdego województwa i przetwarzanie
+wojewodztwa <- c("dolnoslaskie", "kujawsko-pomorskie", "lubelskie", "lubuskie", 
+                 "lodzkie", "malopolskie", "mazowieckie", "opolskie", "podkarpackie", 
+                 "podlaskie", "pomorskie", "slaskie", "swietokrzyskie", "warminsko-mazurskie", 
+                 "wielkopolskie", "zachodniopomorskie")
+
+
+
+
+# Iteracja przez województwa
+for (wojewodztwo in wojewodztwa) {
+  # Sprawdź istnienie pliku dla województwa
+  file_path <- file.path("zad5/Analiza_przestrzenna_5-heads-main/2", paste0(wojewodztwo, ".txt"))
+  cat("Sprawdzam ścieżkę:", file_path, "\n")
+  
+  if (file.exists(file_path)) {
+    woj_txt <- read.table(file_path, header = TRUE, sep = " ")
+    colnames(woj_txt) <- c("x", "y", "z")  # Zakładam, że to są nazwy kolumn w pliku
+    
+    # Konwertowanie na obiekt typu SpatVector
+    spat_vector <- vect(woj_txt, geom = c("x", "y"), crs = "EPSG:2180")
+    
+    # Dodanie trzeciego wymiaru (z)
+    spat_vector$z <- woj_txt$z
+    print(spat_vector)
+    
+    
+    # Obliczanie statystyk dla powiatów
+    powiaty_stats <- calculate_stats(spat_vector, powiaty_2180)
+    
+    # Sprawdzenie zawartości powiaty_stats (debug)
+    print(powiaty_stats)
+    
+    # Sprawdzenie, czy powiaty_stats zawiera dane do zapisu
+    if (nrow(powiaty_stats) > 0) {
+      # Zapisywanie wyników do pliku CSV
+      csv_file <- paste0("zad5/Analiza_przestrzenna_5-heads-main/2/", wojewodztwo, "_powiaty_stats.csv")
+      write.csv(powiaty_stats, file = csv_file, row.names = FALSE)
+      cat("Zapisano wyniki do:", csv_file, "\n")
+    } else {
+      cat("Brak danych do zapisu dla województwa:", wojewodztwo, "\n")
+    }
+    
+  } else {
+    cat("Brak pliku dla województwa:", wojewodztwo, "\n")
+  }
+}
 
